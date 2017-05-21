3bc4e6e72c80c7553fe071c32763dde1e26ee45a
lukaszopara <lukaszopara@gmail.com>
2017-05-21 14:30:39 +0200

    update:mark+edit+delete tasks

--- controller.js
+++ controller.js
@@ -1,34 +1,47 @@
-app.controller('toDoListController', function ($scope){
-    
+app.controller('toDoListController', function ($scope) {
+
     $scope.appTitle = "Lista TODO";
-    $scope.doneTaskLink = "Sprawdź wykonane zadania";
-    $scope.listTaskLink = "Powrót do aktualnych zadań";
-    $scope.deleteTask = "Usuń";
-    $scope.setAsDone = "Zrobione";
-    $scope.setAsToDo = "Do zrobienia";
+    $scope.deleteDoneTask = "Usuń wykonane zadania";
     $scope.addNewTask = "Dodaj nowe zadanie";
-    $scope.saveTaskButton = "Zapisz zadanie";
+    $scope.saveTaskButton = "Zapisz";
     $scope.taskNamePlaceholder = "Nazwa zadania";
     $scope.taskDescriptionPlaceholder = "Opis zadania";
     
     $scope.savedTasks = localStorage.getItem('taskList');
     
-    $scope.taskList = (localStorage.getItem('taskList')!==null) ? JSON.parse($scope.savedTasks) : [ {name: 'Zrobić pranie', desc: 'Trzeba wyprać odświętne ubrania, bo święta, święta i po świętach', done: 0}, {name: 'Wynieść śmieci', desc: 'Tym razem pamiętać o kluczu do wiaty!', done: 1}, {name: 'Remont', desc: 'Pomalować kuchnię i salon', done: 0} ];
-    
-    localStorage.setItem('taskList', JSON.stringify($scope.taskList));
-    
-    $scope.cleanTask = function() {
-        $scope.taskName = '';
-        $scope.taskDesc = '';
+    $scope.taskList = (localStorage.getItem('taskList') !== null) ? JSON.parse($scope.savedTasks) : [{
+        name: 'Zjeść obiad na szczycie Mont Blanc',
+        done: 0
+    }, {
+        name: 'Otworzyć rano oczy',
+        done: 1
+    }, {
+        name: 'Przybić piątkę z rekinem w jego naturalnym środowisku',
+        done: 0
+    }];
+
+    $scope.saveLocalData = function () {
+        localStorage.setItem('taskList', JSON.stringify($scope.taskList));
     }
-    
-    $scope.addTask = function() {
+
+    $scope.saveLocalData();
+
+    $scope.addTask = function () {
         $scope.taskList.push({
             name: $scope.taskName,
-            desc: $scope.taskDesc,
             done: 0
         });
-        $scope.cleanTask();
-        localStorage.setItem('taskList', JSON.stringify($scope.taskList));
+        $scope.taskName = '';
+        $scope.saveLocalData();
     };
+
+    $scope.delete = function () {
+        var oldList = $scope.taskList;
+        $scope.taskList = [];
+        angular.forEach(oldList, function (item) {
+            if (!item.done)
+                $scope.taskList.push(item);
+        });
+        $scope.saveLocalData();
+    }
 });
--- module.js
+++ module.js
@@ -1,1 +1,1 @@
-var app = angular.module('ToDoList', ['ngRoute']);
+var app = angular.module('ToDoList', []);
